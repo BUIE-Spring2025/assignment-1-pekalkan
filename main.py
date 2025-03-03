a1133b6e67f7f3960033008f57195d4a95ad1275
pekalkan <enesfurkan.pekalkan@std.bogazici.edu.tr>
2025-03-04 02:11:57 +0300

    define special numbers

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def int_to_roman(num):
-    """
-    Convert an integer to a Roman numeral.
-
-    :param num: Integer value between 1 and 3999 inclusive.
-    :return: A string representing the Roman numeral of the integer.
-    """
+     # List of integer values and their corresponding Roman numeral symbols
+    roman_values = [
+        (1000, "M"), (900, "CM"), (500, "D"), (400, "CD"),
+        (100, "C"), (90, "XC"), (50, "L"), (40, "XL"),
+        (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")
+    ]
