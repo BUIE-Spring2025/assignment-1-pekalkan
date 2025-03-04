f00d7331f9c2e295fececcd4a0d209af08dfeac0
pekalkan <enesfurkan.pekalkan@std.bogazici.edu.tr>
2025-03-04 13:29:07 +0300

    name change in tests

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 def int_to_roman(num):
-     # List of integer values and their corresponding Roman numeral symbols
-    roman_values = [
-        (1000, "M"), (900, "CM"), (500, "D"), (400, "CD"),
-        (100, "C"), (90, "XC"), (50, "L"), (40, "XL"),
-        (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")
-    ]
+  
+
+
